48b1af34e7baf361680d47609d42e7776f2ba341
fengchang_fight <fengchang_fight@163.com>
2025-09-28 14:57:33 +0800

    image search demo

---
+++ imagesearch.py
@@ -0,0 +1,106 @@
+import torch
+from PIL import Image
+import timm
+from sklearn.preprocessing import normalize
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+from pymilvus import MilvusClient
+import os
+from IPython.display import display
+
+
+
+class FeatureExtractor:
+    def __init__(self, modelname):
+        # Load the pre-trained model
+        self.model = timm.create_model(
+            modelname, pretrained=True, num_classes=0, global_pool="avg"
+        )
+        self.model.eval()
+
+        # Get the input size required by the model
+        self.input_size = self.model.default_cfg["input_size"]
+
+        config = resolve_data_config({}, model=modelname)
+        # Get the preprocessing function provided by TIMM for the model
+        self.preprocess = create_transform(**config)
+
+    def __call__(self, imagepath):
+        # Preprocess the input image
+        input_image = Image.open(imagepath).convert("RGB")  # Convert to RGB if needed
+        input_image = self.preprocess(input_image)
+
+        # Convert the image to a PyTorch tensor and add a batch dimension
+        input_tensor = input_image.unsqueeze(0)
+
+        # Perform inference
+        with torch.no_grad():
+            output = self.model(input_tensor)
+
+        # Extract the feature vector
+        feature_vector = output.squeeze().numpy()
+
+        return normalize(feature_vector.reshape(1, -1), norm="l2").flatten()
+
+
+client = MilvusClient(
+    uri="http://localhost:19530",
+    token="root:Milvus"
+)
+if client.has_collection(collection_name="image_embeddings"):
+    client.drop_collection(collection_name="image_embeddings")
+client.create_collection(
+    collection_name="image_embeddings",
+    vector_field_name="vector",
+    dimension=512,
+    auto_id=True,
+    enable_dynamic_field=True,
+    metric_type="COSINE",
+)
+
+
+
+extractor = FeatureExtractor("resnet34")
+
+root = "data/train"
+insert = True
+if insert is True:
+    for dirpath, foldername, filenames in os.walk(root):
+        for filename in filenames:
+            if filename.endswith(".JPEG"):
+                filepath = dirpath + "/" + filename
+                image_embedding = extractor(filepath)
+                client.insert(
+                    "image_embeddings",
+                    {"vector": image_embedding, "filename": filepath},
+                )
+
+
+query_image = "data/test/Afghan_hound/n02088094_4261.JPEG"
+
+results = client.search(
+    "image_embeddings",
+    data=[extractor(query_image)],
+    output_fields=["filename"],
+    search_params={"metric_type": "COSINE"},
+)
+images = []
+for result in results:
+    for hit in result[:10]:
+        filename = hit["entity"]["filename"]
+        img = Image.open(filename)
+        img = img.resize((150, 150))
+        images.append(img)
+
+width = 150 * 5
+height = 150 * 2
+concatenated_image = Image.new("RGB", (width, height))
+
+for idx, img in enumerate(images):
+    x = idx % 5
+    y = idx // 5
+    concatenated_image.paste(img, (x * 150, y * 150))
+display("query")
+display(Image.open(query_image).resize((150, 150)))
+display("results")
+display(concatenated_image)
